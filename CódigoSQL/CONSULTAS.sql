d547f2d43e4f41e6a32e0eec9753fd4fb1a77c9e
Jessica Botón <jessicaboton@gmail.com>
2021-03-31 12:29:32 -0600

    Adding Query
    Adding query 2 and a new file for other querys

--- CódigoSQL/CONSULTAS.sql
+++ CódigoSQL/CONSULTAS.sql
@@ -28,9 +28,23 @@
     
 /*  2. Desplegar total de votos y porcentaje de votos de mujeres por departamento y país. 
     El ciento por ciento es el total de votos de mujeres por país. (Tip: Todos los porcentajes por departamento de un país deben sumar el 100%) */
+ 
+    SELECT b.nom_e as NombreE, b.anio_e as AnioE, b.pais, b.depto, (b.TotalVotos/b.TotalPais*100) as Porcentaje FROM
+    
+        ( SELECT a.ne as nom_e, a.ae as anio_e, a.pais, a.depto, a.TotalVotos, 
+         SUM(a.TotalVotos) over (partition by a.pais) TotalPais FROM
+        
+            ( SELECT e.nombre as ne, e.anio as ae, p.nombre as pais, d.nombre as depto, SUM(dv.alfabetas + dv.analfabetas) as TotalVotos 
+            FROM ELECCION e, PAIS p, DEPARTAMENTO d, DETALLEVOTOS dv, GENERO g, ELECCIONPARTIDO ep, MUNICIPIO m, REGION r
+            WHERE dv.genero = g.idGenero and g.genero = 'mujeres' and dv.eleccionpartido = ep.idEP and ep.eleccion = e.idEleccion
+            and dv.municipio = m.idMunicipio and m.departamento = d.idDepartamento and d.region = r.idRegion and r.pais = p.idPais
+            GROUP BY d.nombre, p.nombre, e.anio, e.nombre 
+            ORDER BY e.anio, p.nombre, d.nombre ) a -- ( A ) SUBCONSULTA 1: Total de votos por deepartamento y pais
+        
+        ORDER BY a.pais, a.depto ) b -- ( B ) SUBCONSULTA 2: Se mantiene y agrega el total de votos por pais
+        
+    ;
 
-    
-    
 
 /*  3. Desplegar el nombre del país, nombre del partido político y número de alcaldías de los partidos políticos que ganaron más alcaldías por país. */
     
@@ -49,7 +63,7 @@
             AND m.departamento = d.idDepartamento 
             AND d.region = r.idRegion AND r.pais = p.idPais
             GROUP BY  pa.nombre, m.nombre, d.nombre, p.nombre
-            ORDER BY p.nombre, d.nombre, m.nombre, pa.nombre ) a -- ( A ) SUBCONSULTA 1
+            ORDER BY p.nombre, d.nombre, m.nombre, pa.nombre ) a -- ( A ) SUBCONSULTA 1:Total de votos por partido/municipio/departamento/pais
         
         ORDER BY a.pais, a.departamento, a.municipio, a.partido ) b -- ( B ) SUBCONSULTA 2
     
--- CódigoSQL/CONSULTAS2.sql
+++ CódigoSQL/CONSULTAS2.sql
@@ -1,29 +1,61 @@
 /* 4. Desplegar todas las regiones por país en las que predomina la raza indígena. Es decir, hay más votos que las otras razas. */
 
+SELECT b.nom_e as NombreE, b.anio_e as AnioE, b.pais, b.depto, (b.TotalVotos/b.TotalPais*100) as Porcentaje FROM
+    
+        ( SELECT a.ne as nom_e, a.ae as anio_e, a.pais, a.region, a.TotalVotos, 
+         MAX(a.TotalVotos) over (partition by a.pais) TotalPais FROM
+        
+            
+            ( SELECT e.nombre as ne, e.anio as ae, p.nombre as pais, r.nombre as region, SUM(dv.alfabetas + dv.analfabetas) as TotalVotos 
+            FROM ELECCION e, PAIS p, DEPARTAMENTO d, DETALLEVOTOS dv, RAZA ra, ELECCIONPARTIDO ep, MUNICIPIO m, REGION r
+            WHERE dv.raza = ra.idRaza and ra.raza = 'INDIGENAS' and dv.eleccionpartido = ep.idEP and ep.eleccion = e.idEleccion
+            and dv.municipio = m.idMunicipio and m.departamento = d.idDepartamento and d.region = r.idRegion and r.pais = p.idPais
+            GROUP BY r.nombre, p.nombre, e.anio, e.nombre 
+            ORDER BY e.anio, p.nombre, r.nombre) a -- ( A ) SUBCONSULTA 1: Total de votos por departamento y pais
+            
+            ( SELECT e.nombre as ne, e.anio as ae, p.nombre as pais, r.nombre as region, ra.raza, SUM(dv.alfabetas + dv.analfabetas) as TotalVotos 
+            FROM ELECCION e, PAIS p, DEPARTAMENTO d, DETALLEVOTOS dv, RAZA ra, ELECCIONPARTIDO ep, MUNICIPIO m, REGION r
+            WHERE dv.raza = ra.idRaza and dv.eleccionpartido = ep.idEP and ep.eleccion = e.idEleccion
+            and dv.municipio = m.idMunicipio and m.departamento = d.idDepartamento and d.region = r.idRegion and r.pais = p.idPais
+            GROUP BY ra.raza, r.nombre, p.nombre, e.anio, e.nombre 
+            ORDER BY e.anio, p.nombre, r.nombre) a -- ( A ) SUBCONSULTA 1: Total de votos por departamento y pais
+        
+        
+        ORDER BY a.pais, a.region ) b -- ( B ) SUBCONSULTA 2: Se mantiene y agrega el total de votos por pais
+        
+        WHERE b.totalvotos = b.TotalPais;
+        
+        
+        SELECT a.pais, a.departamento, a.municipio, a.partido, a.TotalVotos, 
+        MAX(a.TotalVotos) over (partition by a.municipio,a.departamento) ganador FROM
+        
+            ( SELECT p.nombre AS pais, d.nombre as departamento, m.nombre as municipio, pa.nombre as partido, SUM(dv.alfabetas + dv.analfabetas) AS TotalVotos
+            FROM PARTIDO pa, DETALLEVOTOS dv, MUNICIPIO m, DEPARTAMENTO d, REGION r, PAIS p, ELECCIONPARTIDO ep
+            WHERE dv.eleccionpartido = ep.idEP and ep.partido = pa.idPartido AND dv.municipio = m.idMunicipio 
+            AND m.departamento = d.idDepartamento 
+            AND d.region = r.idRegion AND r.pais = p.idPais
+            GROUP BY  pa.nombre, m.nombre, d.nombre, p.nombre
+            ORDER BY p.nombre, d.nombre, m.nombre, pa.nombre ) a -- ( A ) SUBCONSULTA 1:Total de votos por partido/municipio/departamento/pais
+        
+        ORDER BY a.pais, a.departamento, a.municipio, a.partido
+    
+    
+    ( SELECT e.nombre as ne, e.anio as ae, p.nombre as pais, d.nombre as depto, SUM(dv.alfabetas + dv.analfabetas) as TotalVotos 
+            FROM ELECCION e, PAIS p, DEPARTAMENTO d, DETALLEVOTOS dv, RAZA ra, ELECCIONPARTIDO ep, MUNICIPIO m, REGION r
+            WHERE dv.raza = ra.idRaza and ra.raza = 'INDIGENAS' and dv.eleccionpartido = ep.idEP and ep.eleccion = e.idEleccion
+            and dv.municipio = m.idMunicipio and m.departamento = d.idDepartamento and d.region = r.idRegion and r.pais = p.idPais
+            GROUP BY d.nombre, p.nombre, e.anio, e.nombre 
+            ORDER BY e.anio, p.nombre, d.nombre ) a -- ( A ) SUBCONSULTA 1: Total de votos por departamento y pais
+            
+            ( SELECT e.nombre as ne, e.anio as ae, p.nombre as pais, r.nombre as region, d.nombre as depto, SUM(dv.alfabetas + dv.analfabetas) as TotalVotos 
+            FROM ELECCION e, PAIS p, DEPARTAMENTO d, DETALLEVOTOS dv, RAZA ra, ELECCIONPARTIDO ep, MUNICIPIO m, REGION r
+            WHERE dv.raza = ra.idRaza and ra.raza = 'INDIGENAS' and dv.eleccionpartido = ep.idEP and ep.eleccion = e.idEleccion
+            and dv.municipio = m.idMunicipio and m.departamento = d.idDepartamento and d.region = r.idRegion and r.pais = p.idPais
+            GROUP BY d.nombre, r.nombre, p.nombre, e.anio, e.nombre 
+            ORDER BY e.anio, p.nombre, r.nombre, d.nombre ) a -- ( A ) SUBCONSULTA 1: Total de votos por departamento y pais
 
-/*
-5. Desplegar el porcentaje de mujeres universitarias y hombres universitarios que votaron por departamento, donde las mujeres universitarias que votaron fueron más que los hombres universitarios que votaron. 
-*/
-/*
-6. Desplegar el nombre del país, la región y el promedio de votos por departamento. Por ejemplo: Si la región tiene tres departamentos, se debe sumar todos los votos de la región y dividirlo dentro de tres (número de departamentos de la región). 7. Desplegar el nombre del municipio y el nombre de los dos partidos políticos con más votos en el municipio, ordenados por país. 
-*/
-/*
-8. Desplegar el total de votos de cada nivel de escolaridad (primario, medio, universitario) por país, sin importar raza o sexo. 
-*/
-/*
-9. Desplegar el nombre del país y el porcentaje de votos por raza. 
-*/
-/*
-10. Desplegar el nombre del país en el cual las elecciones han sido más peleadas. Para determinar esto se debe calcular la diferencia de porcentajes de votos entre el partido que obtuvo más votos y el partido que obtuvo menos votos. (La diferencia más pequeña). 
- */
-/*
-11. Desplegar el total de votos y el porcentaje de votos emitidos por mujeres indígenas alfabetas. 
+/* 5. Desplegar el porcentaje de mujeres universitarias y hombres universitarios que votaron por departamento, donde las mujeres universitarias que votaron fueron más que los hombres universitarios que votaron. */
 
-/* 12. Desplegar el nombre del país, el porcentaje de votos de ese país en el que han votado mayor porcentaje de analfabetas. (tip: solo desplegar un nombre de país, el de mayor porcentaje). 
- */
- /*
-13. Desplegar la lista de departamentos de Guatemala y número de votos obtenidos, para los departamentos que obtuvieron más votos que el departamento de Guatemala. 
- */
- /*
-14. Desplegar el total de votos de los municipios agrupados por su letra inicial. Es decir, agrupar todos los municipios con letra A y calcular su número de votos, lo mismo para los de letra inicial B, y así sucesivamente hasta la Z. 
-*/
+
+
+/* 6. Desplegar el nombre del país, la región y el promedio de votos por departamento. Por ejemplo: Si la región tiene tres departamentos, se debe sumar todos los votos de la región y dividirlo dentro de tres (número de departamentos de la región). */
